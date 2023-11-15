42b070b8da652d1b72877a6aedc98e50924757ca
b-chen12 <brianc2002@gmail.com>
2023-11-15 00:44:10 -0600

    added correctness check caliper

--- BitonicSort/MPI/bitonic.cpp
+++ BitonicSort/MPI/bitonic.cpp
@@ -18,6 +18,7 @@ MPI_Status status;
 
 // Define Caliper region names
 const char *main_region = "main";
+const char *data_init = "data_init";
 const char *comm = "comm";
 const char *comm_MPI_Barrier = "comm_MPI_Barrier";
 const char *comm_large = "comm_large";
@@ -25,7 +26,7 @@ const char *comm_large_MPI_Gather = "comm_large_MPI_Gather";
 const char *comm_large_MPI_Scatter = "comm_large_MPI_Scatter";
 const char *comp = "comp";
 const char *comp_large = "comp_large";
-const char *data_init = "data_init";
+const char *correctness_check = "correctness_check";
 
 float random_float() {
     return (float)rand() / (float)RAND_MAX;
